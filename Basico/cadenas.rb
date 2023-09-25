d36ce2aa87bb2d05934ac5c361ece01402e27898
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-09-24 20:40:22 -0600

    Strings - video#3

---
+++ Basico/cadenas.rb
@@ -0,0 +1,13 @@
+nombre = "Belén"
+
+puts "Hola " + nombre
+
+puts "\n2+2=#{2 + 2}"
+
+puts "\nHola #{ nombre.upcase }"
+
+puts "\nHola #{ nombre.downcase }"
+
+puts "\n \tHola " + nombre
+
+puts '\n \t'
